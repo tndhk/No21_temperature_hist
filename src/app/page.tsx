c38cea27f0e3191e3a3bbb83b2506fef6a30bc37
Takahiko Tsunoda <hk.yokohama@gmail.com>
2025-04-26 17:55:07 +0900

    Update function renewal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,6 @@ export default async function Home() {
         <h1 className="text-2xl font-bold">Temperature History Comparison in Tokyo</h1>
 
         <HomePageClient initialDbData={data} availableYears={availableYears} />
-        <ETLButton />
       </main>
     </div>
   );
